ffd1f6cbefefe15cfd3e06497dc64f897141210f
Shoaib Ahmed <sufialhussaini@gmail.com>
2024-12-20 01:48:09 +0400

    Import tests for MerkleTreeVerification

--- src/lib.rs
+++ src/lib.rs
@@ -107,6 +107,118 @@ impl ConstraintSynthesizer<Fq> for MerkleTreeVerification {
 mod test {
     use super::*;
 
+    #[test]
+    fn merkle_tree_constraints_correctness() {
+        use ark_relations::r1cs::ConstraintSystem;
+
+        // Let's set up an RNG for use within tests. Note that this is *not* safe
+        // for any production use.
+        let mut rng = ark_std::test_rng();
+
+        // First, let's sample the public parameters for the hash functions:
+        let leaf_crh_params = <LeafH as CRHScheme>::setup(&mut rng).unwrap();
+        let two_to_one_crh_params = <CompressH as TwoToOneCRHScheme>::setup(&mut rng).unwrap();
+
+        // Next, let's construct our tree.
+        // This follows the API in https://github.com/arkworks-rs/crypto-primitives/blob/6be606259eab0aec010015e2cfd45e4f134cd9bf/src/merkle_tree/mod.rs#L156
+        let tree = JubJubMerkleTree::new(
+            &leaf_crh_params,
+            &two_to_one_crh_params,
+            [1u8, 2u8, 3u8, 10u8, 9u8, 17u8, 70u8, 45u8].map(|u| [u]), // the i-th entry is the i-th leaf.
+        )
+        .unwrap();
+
+        // Now, let's try to generate a membership proof for the 5th item, i.e. 9.
+        let proof = tree.generate_proof(4).unwrap(); // we're 0-indexing!
+                                                     // This should be a proof for the membership of a leaf with value 9. Let's check that!
+
+        // First, let's get the root we want to verify against:
+        let root = tree.root();
+
+        let circuit = MerkleTreeVerification {
+            // constants
+            leaf_crh_params,
+            two_to_one_crh_params,
+
+            // public inputs
+            root,
+            leaf: 9u8,
+
+            // witness
+            authentication_path: Some(proof),
+        };
+
+        // Next, let's make the circuit!
+        let cs = ConstraintSystem::new_ref();
+        circuit.generate_constraints(cs.clone()).unwrap();
+        // Let's check whether the constraint system is satisfied
+        let is_satisfied = cs.is_satisfied().unwrap();
+        if !is_satisfied {
+            // If it isn't, find out the offending constraint.
+            println!("{:?}", cs.which_is_unsatisfied());
+        }
+        assert!(is_satisfied);
+    }
+
+    // Run this test via `cargo test --release test_merkle_tree_constraints_soundness`.
+    // This tests that a given invalid authentication path will fail.
+    #[test]
+    fn merkle_tree_constraints_soundness() {
+        use ark_relations::r1cs::ConstraintSystem;
+
+        // Let's set up an RNG for use within tests. Note that this is *not* safe
+        // for any production use.
+        let mut rng = ark_std::test_rng();
+
+        // First, let's sample the public parameters for the hash functions:
+        let leaf_crh_params = <LeafH as CRHScheme>::setup(&mut rng).unwrap();
+        let two_to_one_crh_params = <CompressH as TwoToOneCRHScheme>::setup(&mut rng).unwrap();
+
+        // Next, let's construct our tree.
+        // This follows the API in https://github.com/arkworks-rs/crypto-primitives/blob/6be606259eab0aec010015e2cfd45e4f134cd9bf/src/merkle_tree/mod.rs#L156
+        let tree = JubJubMerkleTree::new(
+            &leaf_crh_params,
+            &two_to_one_crh_params,
+            [1u8, 2u8, 3u8, 10u8, 9u8, 17u8, 70u8, 45u8].map(|u| [u]), // the i-th entry is the i-th leaf.
+        )
+        .unwrap();
+
+        // We just mutate the first leaf
+        let second_tree = JubJubMerkleTree::new(
+            &leaf_crh_params,
+            &two_to_one_crh_params,
+            [4u8, 2u8, 3u8, 10u8, 9u8, 17u8, 70u8, 45u8].map(|u| [u]), // the i-th entry is the i-th leaf.
+        )
+        .unwrap();
+
+        // Now, let's try to generate a membership proof for the 5th item, i.e. 9.
+        let proof = tree.generate_proof(4).unwrap(); // we're 0-indexing!
+
+        // But, let's get the root we want to verify against:
+        let wrong_root = second_tree.root();
+
+        let circuit = MerkleTreeVerification {
+            // constants
+            leaf_crh_params,
+            two_to_one_crh_params,
+
+            // public inputs
+            root: wrong_root,
+            leaf: 9u8,
+
+            // witness
+            authentication_path: Some(proof),
+        };
+
+        // Next, let's make the constraint system!
+        let cs = ConstraintSystem::new_ref();
+        circuit.generate_constraints(cs.clone()).unwrap();
+        // Let's check whether the constraint system is satisfied
+        let is_satisfied = cs.is_satisfied().unwrap();
+        // We expect this to fail!
+        assert!(!is_satisfied);
+    }
+
     /// Generate a merkle tree, its constraints, and test its constraints
     fn merkle_tree_test(
         leaves: Vec<Vec<u8>>,
